76ad38aa0711fbab9fa25a72f1a6d5d9953745e8
pipopotamasu <ec0156hx@gmail.com>
2022-07-30 14:14:04 -0700

    upvote mockup

--- src/components/Upvote.vue
+++ src/components/Upvote.vue
@@ -2,8 +2,22 @@
 </script>
 
 <template>
-  <h1>Upvote</h1>
+  <button type="button" class="upvote-button">
+    <img src="../assets/arrow-tailup.svg" alt="Arrow Tailup Icon " />
+  </button>
 </template>
 
 <style scoped>
+.upvote-button {
+  cursor: pointer;
+  padding: 0;
+  border: none;
+  border-radius: 4px;
+  background-color: #F4F6F8;
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
